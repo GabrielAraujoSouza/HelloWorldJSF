0ed3ca9064fa4cfcca0340d78f1b86486f0cf72c
GabrielAraujo <souzaa.gabriel@gmail.com>
2016-11-08 08:23:49 -0200

    First project with jsf

---
+++ Financeiro/src/main/java/com/algaworks/financeiro/model/CriarTabelas.java
@@ -0,0 +1,10 @@
+package com.algaworks.financeiro.model;
+
+import javax.persistence.Persistence;
+
+public class CriarTabelas {
+
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("FinanceiroPU");
+	}
+}
